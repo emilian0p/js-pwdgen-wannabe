95d22ba4ec7195c08ff2b527106894a8ae518afa
emilian0p <102601927+emilian0p@users.noreply.github.com>
2022-06-13 16:11:12 +0200

    modifica js

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
+
 const userName = prompt('Inserisci il tuo nome')
 
 const userSurname = prompt('Inserisci il tuo cognome')
 
 const favColor = prompt('Inserisci il tuo colore preferito')
 
-
-
-console.log(userName, userSurname, favColor);
-
-
-
+const password = userName + userSurname + favColor + '22'
 
 document.getElementById('user-name').innerHTML = userName
 
 document.getElementById('user-surname').innerHTML = userSurname
 
 document.getElementById('user-favourite-color').innerHTML = favColor
+
+document.getElementById('password').innerHTML = password
+
+
+console.log(userName, userSurname, favColor);
+console.log(password)
+
